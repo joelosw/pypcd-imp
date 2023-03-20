ec687bea6764958d73301636997b9df657232d0c
joelosw <joelo1996@gmail.com>
2023-03-20 10:34:07 +0100

    update description

--- setup.py
+++ setup.py
@@ -1,16 +1,18 @@
 import os
 from setuptools import setup
-
+from pathlib import Path
 # Get version and release info, which is all stored in pypcd_imp/version.py
 ver_file = os.path.join('pypcd_imp', 'version.py')
 with open(ver_file) as f:
     exec(f.read())
-
+README=Path('README.md').read_text()
 opts = dict(name=NAME,
             maintainer=MAINTAINER,
             maintainer_email=MAINTAINER_EMAIL,
             description=DESCRIPTION,
-            long_description=LONG_DESCRIPTION,
+            #long_description=LONG_DESCRIPTION,
+            long_description_content_type="text/markdown",
+            long_description=README,
             url=URL,
             download_url=DOWNLOAD_URL,
             license=LICENSE,
